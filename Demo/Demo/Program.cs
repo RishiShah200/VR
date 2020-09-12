7f62f3a698b03f9003bfcf5dd88a189e0ea23672
Rishi Shah <36748900+RishiShah200@users.noreply.github.com>
2020-09-12 09:26:12 -0400

    Changes

---
+++ Demo/Demo/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Please enter a string : ");
+            string str1 = Console.ReadLine();
+
+            Console.Write("Please enter a second string : ");
+            var str2 = Console.ReadLine();      //type inferencing. determines the type.
+
+            Console.Write(str1);
+            Console.Write(str2);
+
+            int total = 0;
+
+            do
+            {
+
+                Console.Write("Please enter a number - 0 to stop ");
+                str1 = Console.ReadLine();
+
+
+                try
+                {
+                    
+                    total += Int32.Parse(str1);
+                }
+                catch(FormatException e)
+                {
+                    //Console.WriteLine(e);
+                }
+                
+            } while (!str1.Equals("0"));
+
+            Console.WriteLine(total);
+
+            List<int> list = new List<int>();
+            int x = 1;
+
+            while (x <= 10)
+            {
+                list.Add(x);
+                x++;
+            }
+
+            //Console.WriteLine(list); this does not work
+
+            for(int i = 1; i < list.Count; i++)
+            {
+                Console.WriteLine(list.ElementAt(i) + " or " + list[i]);
+            }
+
+            foreach(int number in list)
+            {
+                Console.WriteLine("Num : {0} is a number {1} is the string we used b4", number, str1);
+            }
+
+            var tupleOne = (1, "apple",2);
+            var tupleTwo = (firstname: "bob", secondname: "smith");
+
+            Console.WriteLine(tupleOne.Item1);
+            Console.WriteLine(tupleTwo.firstname);
+
+            Dictionary<string, string> dictionary = new Dictionary<string, string>();
+            dictionary.Add("E302", "Schiff");
+            dictionary.Add("100", "Miller");
+            dictionary.Add("205", "Hayston");
+
+            string name = "";
+            dictionary.TryGetValue("100", out name);    //if true, adds the value from the dictionary to name
+            Console.WriteLine(name);
+
+        }//main
+
+        public static (string,string) NamePairMaker(string first,string second)
+        {
+            return (first, second);
+        }
+
+    }//class
+}//namespace
---
+++ Practice1/Practice1/Program.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Practice1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            List<int> myList = MakeList();
+            var myTuple = AverageAndTotalNums(myList);
+            Console.WriteLine("Average of List: {0} \nTotal Nums: {1}", myTuple.Item1, myTuple.Item2);
+            FindLongestString();
+
+            Dictionary<string, string> dictionary = new Dictionary<string, string>();
+            dictionary.Add("Math", "Dentler");
+            dictionary.Add("Language", "Kostovny");
+            dictionary.Add("Computer Science", "Schiff");
+            dictionary.Add("Economics", "Galaro");
+            dictionary.Add("Gym", "Webb");
+            dictionary.Add("English", "Lehre");
+
+            WhatSubjects(dictionary);
+            
+
+
+        }
+
+        public static List<int> MakeList()
+        {
+
+            List<int> list = new List<int>();
+            int x = 0;
+            while (list.Count < 500)
+            {
+                if (x % 2 == 0 && x % 3 == 0)
+                {
+                    if (x % 10 != 0)
+                    {
+                        list.Add(x);
+                    }
+                }
+                x++;
+            }
+            return list;
+        }
+
+        public static (double,int) AverageAndTotalNums(List<int> list)
+        {
+            int totalNums = 0;
+            double average = 0.0;
+            int sum = 0;
+            foreach (int number in list)
+            {
+                sum += number;
+                totalNums++;
+            }
+            average = sum / (double)totalNums;
+
+            return (average, totalNums);
+        }
+
+        public static List<(string,int)> FindLongestString()
+        {
+            Console.WriteLine("Enter a string: ");
+            string stringOne = Console.ReadLine();
+
+            Console.WriteLine("Enter a string: ");
+            string stringTwo = Console.ReadLine();
+
+            Console.WriteLine("Enter a string: ");
+            string stringThree = Console.ReadLine();
+
+            Console.WriteLine("Enter a string: ");
+            string stringFour = Console.ReadLine();
+
+            List<string> myList = new List<string>();
+            myList.Add(stringOne);
+            myList.Add(stringTwo);
+            myList.Add(stringThree);
+            myList.Add(stringFour);
+
+            int longestStringLength = myList.ElementAt(0).Length;
+            int index = 0;
+
+            for(int x = 0; x < myList.Count; x++)
+            {
+                if(myList.ElementAt(x).Length > longestStringLength)
+                {
+                    longestStringLength = myList.ElementAt(x).Length;
+                    index = x;
+                }
+            }
+
+            string longestString = myList.ElementAt(index);
+
+            myList.RemoveAt(index);
+
+            int secondLongestStringLength = myList.ElementAt(0).Length;
+            int anotherIndex = 0;
+
+            for (int x = 0; x < myList.Count; x++)
+            {
+                if (myList.ElementAt(x).Length > secondLongestStringLength)
+                {
+                    secondLongestStringLength = myList.ElementAt(x).Length;
+                    anotherIndex = x;
+                }
+            }
+
+            string secondLongestString = myList.ElementAt(anotherIndex);
+
+
+            List<(string,int)> finalList = new List<(string, int)>();
+
+            finalList.Add((longestString, longestStringLength));
+            finalList.Add((secondLongestString, secondLongestStringLength));
+
+            for(int x = 0; x < finalList.Count; x++)
+            {
+                Console.WriteLine(finalList[x]);
+            }
+
+            return finalList;
+
+
+        }
+
+        public static List<string> WhatSubjects(Dictionary<string,string> dictionary)
+        {
+            string mathTeacher = "";
+            string englishTeacher = "";
+            string scienceTeacher = "";
+
+            List<string> teacherList = new List<string>();
+
+            foreach (KeyValuePair<string, string> entry in dictionary)
+            {
+                if (entry.Key.Equals("Math"))
+                {
+                    dictionary.TryGetValue("Math", out mathTeacher);
+                }
+
+                if (entry.Key.Equals("English"))
+                {
+                    dictionary.TryGetValue("English", out englishTeacher);
+                }
+
+                if (entry.Key.Equals("Science"))
+                {
+                    dictionary.TryGetValue("Science", out scienceTeacher);
+                }
+            }
+
+            teacherList.Add(mathTeacher);
+            teacherList.Add(scienceTeacher);
+            teacherList.Add(englishTeacher);
+
+            foreach(string element in teacherList)
+            {
+                Console.WriteLine(element);
+            }
+
+            return teacherList;
+
+        }
+
+
+    }
+}
